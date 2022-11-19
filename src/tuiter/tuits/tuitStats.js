03722300bc608db149a61521225510e099daa23c
Nano Liklikadze <nanoliklikadze@Nanos-MacBook-Pro.local>
2022-11-19 15:51:23 -0500

    finalized

--- src/tuiter/tuits/tuitStats.js
+++ src/tuiter/tuits/tuitStats.js
@@ -57,7 +57,7 @@ const TuitStats = (
             </div>
 
             <div className={"col-2"}>
-                Dislikes: {tuit.dislikes}
+
 
                 <i onClick={() => dispatch(updateTuitThunk({
                                                                ...tuit,
