7c71bb0a1c37f3a5e66d999aa54de041dd953194
Nano Liklikadze <nanoliklikadze@Nanos-MacBook-Pro.local>
2022-12-10 01:15:38 -0500

    switched render

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+// const API_BASE = process.env.REACT_APP_API_BASE;
+// const TUITS_API = `${API_BASE}/tuits`;
+
+const TUITS_API =" https://tuiter-app.onrender.com/api/tuits"
 
 
 
